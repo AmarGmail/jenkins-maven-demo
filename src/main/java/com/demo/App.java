de2f4536f778fda5a412ff2cc75a20756d1199ff
Amarjyoti Lahkar <amarjyotilahkar37@gmail.com>
2026-07-06 08:01:33 +0000

    Add add() method and JUnit test

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -6,8 +6,12 @@ package com.demo;
  */
 public class App 
 {
+    public static int add(int a, int b) {
+        return a + b;
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Addition result: " + add( 10, 20));
     }
 }
